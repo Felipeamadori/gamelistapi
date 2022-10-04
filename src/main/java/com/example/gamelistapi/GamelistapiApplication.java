b8cfc9f007b9346cb7b55a49dc3cc3538472c9c5
Felipeamadori <famachado@inf.ufsm.br>
2022-10-04 20:17:55 -0300

    Criacao do projeto

---
+++ src/main/java/com/example/gamelistapi/GamelistapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamelistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelistapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelistapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gamelistapi/GamelistapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamelistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
