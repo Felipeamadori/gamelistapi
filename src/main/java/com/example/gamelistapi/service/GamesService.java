5fe18f54b3c7f024c03fc69b4f5d3eceb8d38932
SouzaJr2102 <souzajr2102@gmail.com>
2022-10-26 06:27:42 -0300

    adicionando paginacao e ordenacao

--- src/main/java/com/example/gamelistapi/service/GamesService.java
+++ src/main/java/com/example/gamelistapi/service/GamesService.java
@@ -4,9 +4,12 @@ import com.example.gamelistapi.dto.GamesDto;
 import com.example.gamelistapi.model.Games;
 import com.example.gamelistapi.repository.GamesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class GamesService {
